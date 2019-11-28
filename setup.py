4f573eeea5d174fdaa8a74169022be6789f1aa64
Alejandro M. BERNARDIS <alejandro.bernardis@gmail.com>
2019-11-28 12:36:50 -0300

    UPD // Fixes

--- setup.py
+++ setup.py
@@ -38,9 +38,7 @@ setup(
 
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Environment :: Console',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
